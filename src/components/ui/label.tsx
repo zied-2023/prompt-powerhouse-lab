643246b23b0141beb8c493e37fe3aebf38d15cad
zied-2023 <140195504+zied-2023@users.noreply.github.com>
2025-11-02 18:34:06 +0100

    Updated vite.config.ts

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -3,7 +3,7 @@ import * as LabelPrimitive from "@radix-ui/react-label"
 
 import { cn } from "@/lib/utils"
 
-const Label = React.forwardRef
+const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
 >(({ className, ...props }, ref) => (
